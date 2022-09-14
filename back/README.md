ddbbf5350d9877713e0bd6c25ed8d58b24ee8db0
shjung <shjung@kr.canon>
2022-09-14 16:22:51 +0900

    [shjung] 환경구축 requirements.txt 작성

---
+++ back/README.md
@@ -0,0 +1,4 @@
+#Backend 환경설정
+
+pip config set global.trusted-host "pypi.org files.pythonhosted.org pypi.python.org"  
+pip install -r requirements.txt
--- back/flaskapp/app.py
+++ back/flaskapp/app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, g, send_from_directory
 from flask_restx import Api
 from controller.login.controller_login import LOGIN
 from controller.user.controller_user import USER
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../web/', template_folder='../web')
     app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
     api = Api(app)
     api.add_namespace(USER, '/api/user')
@@ -15,5 +15,10 @@ def create_app():
     @app.route('/hello', methods=['GET'])
     def index():
         return 'Hello World!'
+
+    @app.route('/main', methods=['GET'])
+    def catch_all():
+        g.jinja2_test = 'made by gtttpark!'
+        return render_template('index.html')
 
     return app
